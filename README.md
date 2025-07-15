53895d618262d3be0e250f3e395e01c39581a823
Tijn Jacobs <90401199+tijn-jacobs@users.noreply.github.com>
2025-07-15 17:51:06 +0200

    Include sticker

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# ShrinkageTrees <img src="https://img.shields.io/badge/R%3E%3D-4.2-blue" alt="R >= 4.2"> ![License: MIT](https://img.shields.io/badge/license-MIT-green) ![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange)
+# ShrinkageTrees <img src="https://img.shields.io/badge/R%3E%3D-4.2-blue" alt="R >= 4.2"> ![License: MIT](https://img.shields.io/badge/license-MIT-green) ![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange) <img src="sticker/ShrinkageTrees_hex.png" align="right" width="180"/>
+
+
 
 This package provides functions for fitting Horseshoe Trees, Causal Horseshoe Forests, and their more general counterparts: Shrinkage Trees and Causal Shrinkage Forests.  
 
---
+++ sticker/make_sticker.R
@@ -0,0 +1,53 @@
+# File to make and adjust the hexagonal sticker
+
+# load the necessary packages
+library(hexSticker) # hexSticker generator
+library(magick)     # Advanced image processing
+library(sysfonts)   # font selection
+library(tidyverse)
+
+#setwd("/Users/tijnjacobs/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/Documents/GitHub/ShrinkageTrees/sticker")
+
+pine_img <- magick::image_read('vector_graph_logo_high.png')
+
+fonts_df <- font_files()
+
+font_add("Avenir Next", "Avenir Next.ttc")
+font_add("Avenir Next Condensed", "Avenir Next Condensed.ttc")
+font_add("DIN Condensed", "DIN Condensed Bold.ttf")
+font_add("DIN Alternate", "DIN Alternate Bold.ttf")
+font_add("Helvetica Neue", "HelveticaNeue.ttc")
+
+sticker(
+  
+  # title of the package
+  package = "ShrinkageTrees",
+  p_size = 25,
+  p_y = 0.5,
+  
+  # Border thickness
+  h_size = 1.4,
+  
+  # Image
+  subplot = pine_img,  
+  s_x = 1,
+  s_y = 1.15,
+  s_width = 1.4,
+  s_height = 1.4,
+  
+  # Colouring
+  p_color = "#3F7426",
+  h_fill = "#B9DA69",  # background color (feel free to change)
+  h_color = "#3F7426", # border color
+  
+  # Spotlight
+  spotlight = TRUE,
+  l_x = 1.35,
+  l_y = 1.55,
+  l_width = 2,
+  l_alpha = 0.21,
+  
+  filename = "ShrinkageTrees_hex.png",
+  dpi = 600,
+  p_family = "DIN Alternate"
+)
