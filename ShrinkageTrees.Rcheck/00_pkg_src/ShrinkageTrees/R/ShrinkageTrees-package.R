6f75fa1c0793f60079c73ae4e221ed778a79833d
Tijn Jacobs <90401199+tijn-jacobs@users.noreply.github.com>
2026-05-22 17:34:40 +0200

    update

--- ShrinkageTrees.Rcheck/00_pkg_src/ShrinkageTrees/R/ShrinkageTrees-package.R
+++
@@ -1,8 +0,0 @@
-#' @keywords internal
-#' @importFrom stats median quantile pnorm sd
-#' @importFrom utils head
-"_PACKAGE"
-
-# Suppress R CMD check NOTE for .data used inside ggplot2 aes() calls.
-utils::globalVariables(".data")
-
--- ShrinkageTrees.Rcheck/00_pkg_src/ShrinkageTrees/src/TruncatedNormal.h
+++
@@ -1,8 +0,0 @@
-#ifndef TruncatedNormal_h
-#define TruncatedNormal_h
-
-#include "Prerequisites.h"
-
-double rtnorm(double mean, double truncation, double sd, Random& random);
-
-#endif
